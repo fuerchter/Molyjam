b9a0e8f2cd8e5ce0487d225f1ce14fa97a9b8cd4
Adrian Wagner <aw@commit.github>
2013-07-06 16:11:48 +0200

    Added Entity base and standard main

---
+++ Entity.lua
@@ -0,0 +1,25 @@
+Entity = {}
+Entity.__index = Entity
+
+setmetatable(Entity, {
+	__call = function (cls, ...)
+				local self = setmetatable({}, cls)
+				self:_init(...)
+				return self
+			end,
+})
+
+--creates an entity
+function Entity:_init(entType, position)
+	self.type = entType
+	self.position = position
+end
+
+--functions that every entity need
+function Entity:update(dt)
+	
+end
+
+function Entity:draw()
+	
+end
